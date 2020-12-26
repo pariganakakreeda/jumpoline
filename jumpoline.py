35ba4cdcf2fe75101b00616b1eafa2ff609c22d4
Hamnah Sabry <hamnahsabry@gmail.com>
2020-12-26 16:48:04 +0100

    including girl figure

--- jumpoline.py
+++ jumpoline.py
@@ -7,10 +7,12 @@ pygame.mouse.set_visible(False)
 
 
 background1_png = pygame.image.load('background1.png')
-human1_png = pygame.image.load('human1.png')
 trampoline_png = pygame.image.load('trampoline.png')
+g1_png = pygame.image.load('g1.png')
+g2_png = pygame.image.load('g2.png')
 
-
+g1_pos_x = 525
+g1_pos_y = 100
 
 while True:
     for event in pygame.event.get():
@@ -19,6 +21,11 @@ while True:
         sys.exit()
 
     screen.blit(background1_png,(0,0))
-    screen.blit(human1_png,(70,70))
+    #screen.blit(human1_png,(370,70))
+    screen.blit(trampoline_png,(200,350))
+    screen.blit(g1_png,(g1_pos_x,g1_pos_y))
+   # screen.blit(human2_png,(370,70))
+
+
     pygame.display.update()
     clock.tick(120)
