d8f5747c776adbb2f9edd3c1dd300b8dded02c3e
Hamnah Sabry <hamnahsabry@gmail.com>
2020-12-31 20:10:29 +0100

    wierd girl with start eyes jums on love

--- jumpoline.py
+++ jumpoline.py
@@ -7,19 +7,19 @@ pygame.mouse.set_visible(False)
 
 
 background4_png = pygame.image.load('background4.png')
-trampoline3_png = pygame.image.load('trampoline3.png')
+trampoline5_png = pygame.image.load('trampoline5.png')
 g1_png = pygame.image.load('g1.png')
 g2_png = pygame.image.load('g2.png')
 #cloud1_png = pygame.image.load('cloud1.png')
 clouds_png = pygame.image.load('clouds.png')
+g4_png = pygame.image.load('g4.png')
 
+g4_pos_x = 160
+g4_pos_y = 110
+g4_top_max_pos = 50
+g4_bottom_max_pos = g4_pos_y
 
-g1_pos_x = 170
-g1_pos_y = 150
-g1_top_max_pos = 50
-g1_bottom_max_pos = g1_pos_y
-
-speed= -5
+speed= -10
 
 
 while True:
@@ -29,15 +29,15 @@ while True:
         sys.exit()
 
     screen.blit(background4_png,(0,0))
-    screen.blit(trampoline3_png,(25,265))
+    screen.blit(trampoline5_png,(0,0))
     #screen.blit(cloud1_png,(40,30))
     #screen.blit(cloud1_png,(900,10))
     screen.blit(clouds_png,(0,0))
-    screen.blit(g1_png,(g1_pos_x,g1_pos_y))
+    screen.blit(g4_png,(g4_pos_x,g4_pos_y))
 
-    if g1_pos_y <= g1_top_max_pos or g1_pos_y > g1_bottom_max_pos :
+    if g4_pos_y <= g4_top_max_pos or g4_pos_y > g4_bottom_max_pos :
        speed = speed * -1
-    g1_pos_y = g1_pos_y + speed
+    g4_pos_y = g4_pos_y + speed
 
     #if g1_pos_y > g1_top_max_pos:
        #g1_pos_y = g1_pos_y - 1
