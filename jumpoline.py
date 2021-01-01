d3d20e84f69db43ecc6ba529939915d63e93636c
Hamnah Sabry <hamnahsabry@gmail.com>
2021-01-01 17:41:32 +0100

    move trampoline with mouse

--- jumpoline.py
+++ jumpoline.py
@@ -7,17 +7,21 @@ pygame.mouse.set_visible(False)
 
 
 background4_png = pygame.image.load('background4.png')
-trampoline5_png = pygame.image.load('trampoline5.png')
+trampoline3_png = pygame.image.load('trampoline3.png')
 g1_png = pygame.image.load('g1.png')
 g2_png = pygame.image.load('g2.png')
 #cloud1_png = pygame.image.load('cloud1.png')
 clouds_png = pygame.image.load('clouds.png')
-g4_png = pygame.image.load('g4.png')
+#g4_png = pygame.image.load('g4.png')
+t1_pos_x = 10
+t1_pos_y = 550
 
-g4_pos_x = 160
-g4_pos_y = 110
-g4_top_max_pos = 50
-g4_bottom_max_pos = g4_pos_y
+trampoline_rect=(t1_pos_x,t1_pos_y)
+
+g1_pos_x = 160
+g1_pos_y = 110
+g1_top_max_pos = 50
+g1_bottom_max_pos = g1_pos_y
 
 speed= -10
 
@@ -27,17 +31,19 @@ while True:
       if event.type == pygame.QUIT:
         pygame.quit()
         sys.exit()
-
+      if event.type == pygame.MOUSEMOTION:
+        x, y = event.pos
+        trampoline_rect = trampoline3_png.get_rect(center =(x, t1_pos_y))
+        #print(x)
+        #t1_pos_x = x
     screen.blit(background4_png,(0,0))
-    screen.blit(trampoline5_png,(0,0))
-    #screen.blit(cloud1_png,(40,30))
-    #screen.blit(cloud1_png,(900,10))
+    screen.blit(trampoline3_png,(trampoline_rect))
     screen.blit(clouds_png,(0,0))
-    screen.blit(g4_png,(g4_pos_x,g4_pos_y))
+    screen.blit(g1_png,(g1_pos_x,g1_pos_y))
 
-    if g4_pos_y <= g4_top_max_pos or g4_pos_y > g4_bottom_max_pos :
+    if g1_pos_y <= g1_top_max_pos or g1_pos_y > g1_bottom_max_pos :
        speed = speed * -1
-    g4_pos_y = g4_pos_y + speed
+    g1_pos_y = g1_pos_y + speed
 
     #if g1_pos_y > g1_top_max_pos:
        #g1_pos_y = g1_pos_y - 1
